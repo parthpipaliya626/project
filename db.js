56c1f0614e30e34b00d48037584a43b86c5d68a4
parthpipaliya626 <ppipaliya626@rku.ac.in>
2023-08-10 18:05:04 +0530

    add login and verification function

--- db.js
+++ db.js
@@ -1,48 +1,14 @@
-// const { pool } = require("pg");
-// const dotenv = require("dotenv");
-// dotenv.config()
- 
-// const connectDb = async () => {
-//     try {
-//         const pool = new pool({
-//             user: process.env.PGUSER,
-//             host: process.env.PGHOST,
-//             database: process.env.PGDATABASE,
-//             password: process.env.PGPASSWORD,
-//             port: process.env.PGPORT
-//         })
- 
-//         await pool.connect()
-//         const res = await pool.query('SELECT * FROM pool')
-//         console.log(res)
-//         await pool.end()
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
- 
-// connectDb();
+const Pool = require("pg").Pool;
 
-
-const express = require('express');
-const { Client } = require('pg');
-
-const app = express();
-
-app.use(express.json());
-
-const client = new Client();
-await client.connect();
-
-app.post('/signup', async (req, res) => {
-  const { username, first_name, last_name, password, email } = req.body;
-  const query = 'INSERT INTO users (username, first_name, last_name, password, email) VALUES ($1, $2, $3, hash_password($4), $5)';
-  const values = [username, first_name, last_name, password, email];
-
-  await client.query(query, values);
-  res.json({ message: 'User created successfully' });
+//database conncetion
+const pool = new Pool({
+    user: process.env.db_user,
+    host: process.env.db_host,
+    database: "HRMS",
+    password: process.env.db_pass,
+    port: 5432,
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+module.exports = {
+    query: async(text, params) => await pool.query(text, params),
+};
